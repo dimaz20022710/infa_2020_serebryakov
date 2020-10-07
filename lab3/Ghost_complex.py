0aab82f3711ae83a5ae80b68a5206809c60f2795
Dima <dimaz.20022710>
2020-10-07 14:13:43 +0300

    refactored

--- lab3/Ghost_complex.py
+++ lab3/Ghost_complex.py
@@ -11,6 +11,7 @@ screen_width = 500
 screen_height = 700
 screen = pygame.display.set_mode((screen_width, screen_height))
 
+# color definition
 brown = (75, 45, 0)
 pale_transparent = (215, 215, 255, 210)
 black = (0, 0, 0)
@@ -19,11 +20,15 @@ window_color = (40, 70, 50)
 dark_gray = (20, 22, 20)
 yellow = (255, 255, 0)
 
-d.rect(screen, (100, 100, 100), (0, 0, 500, 300))
-d.rect(screen, black, (0, 300, 500, 400))
+
+def background():
+    """function draws a background view"""
+    d.rect(screen, (100, 100, 100), (0, 0, screen_width, 3 * screen_height // 7))
+    d.rect(screen, black, (0, 300, screen_width, 4 * screen_height // 7))
 
 
 def coinflip(length):
+    """returns a series of ones & zeros with equal probability with custom length"""
     A = []
     for i in range(length):
         if (rnd.random() - 0.5) > 0:
@@ -33,24 +38,23 @@ def coinflip(length):
     return A
 
 
-# function takes only the shape of the ghost from image, else is done via pygame tools
 def ghost(x, y, scale=1.0, orientation=True):
+    """function takes only the shape of the ghost from image, else is done via pygame tools"""
     if scale > 1:
-        return print('error: scale <= 1')
+        print('error: scale <= 1')
     else:
         s = pygame.Surface((screen_width, screen_height), pygame.SRCALPHA)
         pixAr = pygame.PixelArray(s)
         A = []
         B = []
-        with Image.open("ghostimage.png") as im:
-            width, height = im.size
-            for x_pix in range(width):
-                for y_pix in range(height):
-                    r, g, b, a = im.getpixel((x_pix, y_pix))
-                    if r >= 10 and g >= 10 and b >= 10:
-                        A.append(x + int(x_pix * scale))
-                        B.append(y + int(y_pix * scale))
-        print(len(A), len(B))
+        im = Image.open("ghostimage.png")
+        width, height = im.size
+        for x_pix in range(width):
+            for y_pix in range(height):
+                r, g, b, a = im.getpixel((x_pix, y_pix))
+                if r >= 10 and g >= 10 and b >= 10:
+                    A.append(x + int(x_pix * scale))
+                    B.append(y + int(y_pix * scale))
         for i in range(len(A)):
             pixAr[A[i]][B[i]] = pale_transparent
         del pixAr
@@ -70,16 +74,18 @@ def ghost(x, y, scale=1.0, orientation=True):
 
 
 def moon(x, y, R=75):
+    """function that draws a moon centred on (x, y)"""
     d.circle(screen, (240, 240, 240), (x, y), R)
     for i in range(R // 3):
-        randx = x + 1.4 * (rnd.random() - 0.5) * R
-        randx = round(randx)
-        randy = y + 1.4 * (rnd.random() - 0.5) * R
-        randy = round(randy)
-        d.circle(screen, (200, 200, 200), (randx, randy), 5 + round(5 * rnd.random()))
+        randx = x + 1.2 * (rnd.random() - 0.5) * R
+        randx = int(round(randx))
+        randy = y + 1.2 * (rnd.random() - 0.5) * R
+        randy = int(round(randy))
+        d.circle(screen, (175, 175, 175), (randx, randy), 5 + int(round(5 * rnd.random())))
 
 
 def cobweb(x, y, scale=1.0):
+    """function that draws spider webs on houses centred on (x, y)"""
     R = 50 * scale
     for i in range(8):
         ymod = round(y + (R * np.sin(i * np.pi / 4)))
@@ -93,20 +99,54 @@ def cobweb(x, y, scale=1.0):
 
 
 def house(x, y, scale=1.0):
+    """function draws a house with (x, y) coordinates of top left corner"""
     width = int(220 * scale)
     height = int(130 * scale)
+    windows(x, y, width, height)
+    top_windows(x, y, width, height)
+    vertical_beams(x, y, width, height, scale)
+    tubes(x, y, width, height)
     d.rect(screen, brown, (x, y, width, height))
-    for i in range(11):
-        if i % 2 == 0:
-            clr = brown
-        else:
-            clr = window_color
-        d.rect(screen, clr, (x + i * width // 11, y - height, width // 11, height))
     d.rect(screen, dark_gray, (x - int(20 * scale), y + 5, int(width + 40 * scale), height // 8))
-    for i in range(7):
-        d.rect(screen, dark_gray, (x - int(20 * scale) + int((width + 40 * scale) * (2 * i + 0.25) / 13),
-                                   y + 5 - height // 4, width // 26, height // 4))
     d.rect(screen, dark_gray, (x - int(20 * scale), y + 5 - height // 4, int(width + 40 * scale), height // 12))
+    d.rect(screen, dark_gray, (x, y - height, width, height // 6))
+    d.polygon(screen, dark_gray, ((x - int(30 * scale), y - height + height // 6),
+                                  (x, y - height + height // 6), (x, y - height)))
+    d.polygon(screen, dark_gray, ((x + width + int(30 * scale), y - height + height // 6),
+                                  (x + width, y - height + height // 6), (x + width, y - height)))
+    cords = [(x + int(22 * scale), y + int(35 * scale)), (x + int(195 * scale), y + int(105 * scale)),
+             (x + int(40 * scale), y - int(90 * scale)), (x + int(180 * scale), y - int(50 * scale))]
+    probs = coinflip(4)
+    for i in range(len(cords)):
+        if probs[i] == 1:
+            a, b = cords[i]
+            cobweb(a, b, 0.45 * scale)
+
+
+def cloud(x, y, grayness=0.0, scale=1.0):
+    """function that draws a cloud with left top corner (x, y) with color scaling by grayness parameter [0, 1]"""
+    s = pygame.Surface((screen_width, screen_height), pygame.SRCALPHA)
+    d.ellipse(s, (255 * grayness, 255 * grayness, 255 * grayness, 100),
+              (x, y, 120 * scale, 25 * scale))
+    d.circle(s, (255 * grayness, 255 * grayness, 255 * grayness, 100),
+             (x + int(120 * 0.8 * scale) // 3, y + int(25 * scale) // 3), 40)
+    d.circle(s, (255 * grayness, 255 * grayness, 255 * grayness, 100),
+             (x + int(120 * 1.5 * scale) // 3, y + int(25 * 0.8 * scale) // 3), 40)
+    d.circle(s, (255 * grayness, 255 * grayness, 255 * grayness, 100),
+             (x + int(120 * 2.4 * scale) // 3, y + int(25 * 1.2 * scale) // 3), 40)
+    d.circle(s, (255 * grayness, 255 * grayness, 255 * grayness, 100),
+             (x + int(120 * 0.95 * scale) // 3, y + int(25 * 2 * scale) // 3), 40)
+    d.circle(s, (255 * grayness, 255 * grayness, 255 * grayness, 100),
+             (x + int(120 * 1.7 * scale) // 3, y + int(25 * 1.7 * scale) // 3), 40)
+    d.circle(s, (255 * grayness, 255 * grayness, 255 * grayness, 100),
+             (x + int(120 * 2.8 * scale) // 3, y + int(25 * 1.3 * scale) // 3), 40)
+    d.circle(s, (255 * grayness, 255 * grayness, 255 * grayness, 100),
+             (x + 25, y + int(12 * scale)), 45)
+    screen.blit(s, (0, 0))
+
+
+def windows(x, y, width, height):
+    """function draws 3 windows centred in the middle of the house"""
     flp = coinflip(3)
     for i in range(3):
         if flp[i] == 1:
@@ -114,32 +154,33 @@ def house(x, y, scale=1.0):
         else:
             clr = window_color
         d.rect(screen, clr, (x + width * (2 * i + 1) // 7, y + height // 3, width // 7, height // 3))
-    d.rect(screen, dark_gray, (x, y - height, width, height // 6))
-    d.polygon(screen, dark_gray, ((x - int(30 * scale), y - height + height // 6),
-                                  (x, y - height + height // 6), (x, y - height)))
-    d.polygon(screen, dark_gray, ((x + width + int(30 * scale), y - height + height // 6),
-                                  (x + width, y - height + height // 6), (x + width, y - height)))
+
+
+def top_windows(x, y, width, height):
+    """function draws 6 second floor windows centred in the middle of the house"""
+    for i in range(11):
+        if i % 2 == 0:
+            clr = brown
+        else:
+            clr = window_color
+        d.rect(screen, clr, (x + i * width // 11, y - height, width // 11, height))
+
+
+def vertical_beams(x, y, width, height, scale):
+    """function draws 7 beams for the balcony of the house"""
+    for i in range(7):
+        d.rect(screen, dark_gray, (x - int(20 * scale) + int((width + 40 * scale) * (2 * i + 0.25) / 13),
+                                   y + 5 - height // 4, width // 26, height // 4))
+
+
+def tubes(x, y, width, height):
+    """function draws a random number of house chimneys on the its root"""
     for i in range(int(6 * rnd.random())):
         d.rect(screen, (40, 42, 40), (x + int(width * rnd.random()), y - height + int(0.12 * height * rnd.random()),
                                       int(width / 10 * rnd.random()) + 3, -int(height / 3 * rnd.random()) - 20))
-    cords = [(x + int(22 * scale), y + int(35 * scale)), (x + int(195 * scale), y + int(105 * scale)),
-             (x + int(40 * scale), y - int(90 * scale)), (x + int(180 * scale), y - int(50 * scale))]
-    probs = coinflip(4)
-    for i in range(len(cords)):
-        if probs[i] == 1:
-            a, b = cords[i]
-            print(a, b)
-            cobweb(a, b, 0.45 * scale)
 
 
-def cloud(x, y, grayness=0.0, scale=1.0):
-    s = pygame.Surface((screen_width, screen_height), pygame.SRCALPHA)
-    d.ellipse(s, (255 * grayness, 255 * grayness, 255 * grayness, 100),
-              (x, y, 120 * scale, 25 * scale))
-    screen.blit(s, (0, 0))
-
-
-
+background()
 moon(420, 100, R=60)
 cloud(270, 200, grayness=0.65, scale=2.5)
 house(330, 280, scale=0.7)
@@ -147,7 +188,7 @@ house(150, 370, scale=0.7)
 house(30, 500, scale=0.7)
 cloud(150, 280, grayness=0.85, scale=2.4)
 cloud(40, 350, grayness=0.55, scale=2.1)
-cloud(150, 80, grayness=0.8, scale=2.8)
+cloud(150, 80, grayness=0.2, scale=2.8)
 cloud(160, 430, grayness=0.7, scale=2.6)
 cloud(90, 120, grayness=0.45, scale=2.2)
 cloud(50, 190, grayness=0.75, scale=2.7)
